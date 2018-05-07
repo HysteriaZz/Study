af4919037fcf998b62b24e916fec6bfcb74efc68
kailin.guo <kailin.guo@geely.com>
2018-05-07 19:48:40 +0800

    thinkin 并发 同步队列

---
+++ thinkin/src/chapter21/part5/TestBlockingQueues.java
@@ -0,0 +1,82 @@
+package chapter21.part5;
+
+import chapter21.part2.LiftOff;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+
+/**
+ * Created by KaiLin.Guo on 2018-05-07.
+ * 使用同步队列解决任务互操作问题
+ * LinkedBlockingQueue无界队列
+ * ArrayBlockingQueue固定大小
+ * SynchronousQueue大小为1
+ */
+public class TestBlockingQueues {
+    static void getKey() {
+        try {
+            new BufferedReader(new InputStreamReader(System.in)).readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static void getKey(String message) {
+        System.out.println(message);
+        getKey();
+    }
+
+    static void test(String msg, BlockingQueue<LiftOff> queue) {
+        System.out.println(msg);
+        LiftOffRunner runner = new LiftOffRunner(queue);
+        Thread t = new Thread(runner);
+        t.start();
+        for (int i = 0; i < 5; i++)
+            runner.add(new LiftOff(5));
+        getKey("Press 'Enter' (" + msg + ")");
+        t.interrupt();
+        System.out.println("Finished " + msg + " test");
+    }
+
+    public static void main(String[] args) {
+        test("LinkedBlockingQueue", new LinkedBlockingQueue<>());
+        test("ArrayBlockingQueue", new ArrayBlockingQueue<>(3));
+        test("SynchronousQueue", new SynchronousQueue<>());
+    }
+
+}
+
+class LiftOffRunner implements Runnable {
+    private BlockingQueue<LiftOff> rockets;
+
+    public LiftOffRunner(BlockingQueue<LiftOff> rockets) {
+        this.rockets = rockets;
+    }
+
+    public void add(LiftOff lo) {
+        try {
+            rockets.put(lo);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted during put()");
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                // Retrieves and removes the head of this queue, waiting if necessary until an element becomes available.
+                LiftOff rocket = rockets.take();
+                rocket.run();
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Waking from take()");
+        }
+        System.out.println("Exiting LiftOffRunner");
+    }
+}
